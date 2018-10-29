7d87014a9a0168db4651c9ece1411b32d5faeec3
zhenghao <zhenghao@cmss.chinamobile.com>
2018-10-29 18:09:42 +0800

    process.cwd()路径bug
    process.cwd()路径bug

--- src/config/defaultConfig.js
+++ src/config/defaultConfig.js
@@ -1,5 +1,10 @@
 module.exports = {
     hostName: '127.0.0.1',
     port: '8888',
+    /*
+    * process.cwd() 是指的当前运行node的路径
+    * 举例：如果在app.js下执行node app.js，那么process.cwd() = /Users/zhenghao/Documents/vue-mall/src
+    * 如果在vue-mall目录下执行node src/app.js ，那么process.cwd() = /Users/zhenghao/Documents/vue-mall/
+    * */
     root: process.cwd()  // 当前文件夹，相当于linux pwd
 }
